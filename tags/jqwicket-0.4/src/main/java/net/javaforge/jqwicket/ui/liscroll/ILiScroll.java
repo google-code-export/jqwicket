b396b6805d20d4040b18d9dfb4243a2808fe4883
max@javaforge.net <max@javaforge.net@57ee0337-2cae-a1d2-7317-812cb19e8380>
2011-02-12 11:57:12 +0000

    [maven-release-plugin]  copy for tag jqwicket-0.4

---
+++ tags/jqwicket-0.4/src/main/java/net/javaforge/jqwicket/ui/liscroll/ILiScroll.java
@@ -0,0 +1,13 @@
+package net.javaforge.jqwicket.ui.liscroll;
+
+import net.javaforge.jqwicket.IJQWidget;
+
+/**
+ * 
+ * @author cschaedel
+ */
+public interface ILiScroll extends IJQWidget<LiScrollOptions> {
+
+	static final CharSequence JQ_COMPONENT_NAME = "liScroll";
+	
+}
